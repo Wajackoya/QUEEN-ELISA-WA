ae6ba9c533e00c2ac58f98f6233e5acfc7f5f245
Wajackoya <141861669+Wajackoya@users.noreply.github.com>
2025-01-31 00:42:28 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,23 +28,23 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   LANGUAGE: 'EN',
   PORT: process.env.PORT || 3000,
   BOT_NAME: 'queen elisa',
-  OWNER_NAME: 'MrNima',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
-  WORK_TYPE: 'private', // ex: public/private
+  OWNER_NAME: 'WAJACKS',
+  OWNER_NUMBER: '254102510747',
+  SUDO: '254111659469',
+  WORK_TYPE: 'public', // ex: public/private
   WORK_MODE: 'all', // ex: all/only_groups/only_pm
-  TIME_ZONE: 'Asia/Colombo',
+  TIME_ZONE: 'Africa/Nairobi',
   CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ\nmr-nima.vercell.app',
   STICKER_AUTHOR: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
   STICKER_PACK: 'QUEEN ELISA',
   REMOVE_BG_APIKEY: '',
   MAXIMUM_FILESIZE: 90, //size in MB
   OWNER_R_EMOJI: '💻',
-  ALIVE_AS_GIF: false,//this only work for short videos
+  ALIVE_AS_GIF: true,//this only work for short videos
   AUTO_VOICE: true,
   INBOX_BLOCK: false,
   OWNER_REACT: true,
-  AUTO_REACT: true,
+  AUTO_REACT: false,
   ONLY_PM_GROUP_MSG_SEND: true,// you want WORK_MODE message send
   AUTO_SEEN_STATUS: true,
   BOT_OFFLINE: false,// Bot online status
@@ -52,8 +52,8 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ANTI_MSG_SPAM: false,
   ANTI_BAD_WORD: false,
   ANTI_VIEW_ONES: false, // This not working now !
-  ANTI_DELETE: true,
-  BANED_USER_MSG_SEND: false,
+  ANTI_DELETE: false,
+  BANED_USER_MSG_SEND: true,
   BLOCK_CMD_MSG_SEND: true,
   INBOX_BLOCK_MSG_SEND: true,
   CMD_CORRECT: true,
